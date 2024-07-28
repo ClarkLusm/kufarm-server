d3f404645e4800d0679494dcd67eb4bf82e11b43
Thanhpv <thanhpv@inso.vn>
2024-07-28 22:48:32 +0700

    Update UI login form

--- src/client/pages/login.tsx
+++ src/client/pages/login.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, Form, notification } from 'antd';
+import { Button, Card, Col, Divider, Form, Row, notification } from 'antd';
 import { Formik, Form as FormikForm, FastField } from 'formik';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
@@ -53,64 +53,64 @@ const Login: NextPage = (props) => {
   }
 
   return (
-    <Formik
-      key="login-form"
-      enableReinitialize
-      initialValues={{}}
-      onSubmit={handleSubmit}
-    >
-      {(formik) => {
-        const {
-          values,
-          touched,
-          errors,
-          isSubmitting,
-          isValid,
-          getFieldProps,
-          setFieldValue,
-        } = formik;
+    <Row>
+      <Col span={8} offset={8}>
+        <Card title="Đăng nhập" bordered={false}>
+          <Formik
+            key="login-form"
+            enableReinitialize
+            initialValues={{}}
+            onSubmit={handleSubmit}
+          >
+            {(formik) => {
+              const {
+                values,
+                touched,
+                errors,
+                isSubmitting,
+                isValid,
+                getFieldProps,
+                setFieldValue,
+              } = formik;
 
-        return (
-          <div className="bg-gray-100">
-            {contextHolder}
-            <div className="bg-white pt-24 pb-4 px-24 m-auto max-w-screen-sm min-h-screen">
-              <div className="flex flex-col">
-                <h1 className="text-3xl">Đăng nhập</h1>
-                <hr className="my-4" />
-                <FormikForm>
-                  <Form layout="vertical" component="div">
-                    <FastField
-                      required
-                      {...getFieldProps('username')}
-                      label="Tên đăng nhập"
-                      component={InputField}
-                      disabled={isSubmitting}
-                    />
-                    <FastField
-                      required
-                      {...getFieldProps('password')}
-                      label="Mật khẩu"
-                      type="password"
-                      component={InputField}
-                      disabled={isSubmitting}
-                    />
-                    <Form.Item>
-                      <Button
-                        type="primary"
-                        htmlType="submit"
-                        className="mt-4 w-full"
-                      >
-                        Submit
-                      </Button>
-                    </Form.Item>
-                  </Form>
-                </FormikForm>
-              </div>
-            </div>
-          </div>
-        );
-      }}
-    </Formik>
+              return (
+                <div className="mt-10">
+                  {contextHolder}
+                  <FormikForm>
+                    <Form layout="vertical" component="div">
+                      <FastField
+                        required
+                        {...getFieldProps('username')}
+                        label="Tên đăng nhập"
+                        component={InputField}
+                        disabled={isSubmitting}
+                      />
+                      <FastField
+                        required
+                        {...getFieldProps('password')}
+                        label="Mật khẩu"
+                        type="password"
+                        component={InputField}
+                        disabled={isSubmitting}
+                      />
+                      <Form.Item>
+                        <Button
+                          type="primary"
+                          htmlType="submit"
+                          className="mt-4 w-full"
+                        >
+                          Đăng nhập
+                        </Button>
+                      </Form.Item>
+                    </Form>
+                  </FormikForm>
+                </div>
+              );
+            }}
+          </Formik>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 

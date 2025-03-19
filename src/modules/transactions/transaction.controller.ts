539d18a6b20ba946f3b81a67425545b569f4367a
Thanh Phan <phanthanh.humg@gmail.com>
2025-03-19 22:54:57 +0700

    feat: Query transactions by email

--- src/modules/transactions/transaction.controller.ts
+++ src/modules/transactions/transaction.controller.ts
@@ -26,31 +26,7 @@ export class TransactionController {
       );
       delete query.toDate;
     }
-    const [data, total] = await this.service.getAll(
-      query,
-      {
-        id: true,
-        txHash: true,
-        amount: true,
-        exchangeRate: true,
-        coin: true,
-        walletBalance: true,
-        status: true,
-        createdAt: true,
-        updatedAt: true,
-        user: {
-          email: true,
-          walletAddress: true,
-        },
-        paymentWallet: {
-          walletAddress: true,
-        },
-      },
-      {
-        user: true,
-        paymentWallet: true,
-      },
-    );
+    const [data, total] = await this.service.getList(query);
     return {
       data,
       total,
--- src/modules/transactions/transaction.service.ts
+++ src/modules/transactions/transaction.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DataSource, Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 
+import { buildQueryFilter } from '../../common/helpers/query-builder';
 import { BaseService } from '../../common/base/base.service';
 import { Transaction } from './transaction.entity';
 
@@ -12,5 +13,40 @@ export class TransactionService extends BaseService<Transaction> {
     public repository: Repository<Transaction>,
   ) {
     super(repository);
+  }
+
+  getList(query?: any): Promise<[Transaction[], number]> {
+    const qr = buildQueryFilter(query);
+    qr.select = {
+      id: true,
+      txHash: true,
+      amount: true,
+      exchangeRate: true,
+      coin: true,
+      walletBalance: true,
+      status: true,
+      createdAt: true,
+      updatedAt: true,
+      user: {
+        email: true,
+        walletAddress: true,
+      },
+      paymentWallet: {
+        walletAddress: true,
+      },
+    };
+    qr.relations = {
+      user: true,
+      paymentWallet: true,
+    };
+    if (query.email) {
+      qr.where = {
+        user: {
+          email: ILike(`%${query.email}%`),
+        },
+      };
+    }
+    delete qr.where['email'];
+    return this.repository.findAndCount(qr);
   }
 }
